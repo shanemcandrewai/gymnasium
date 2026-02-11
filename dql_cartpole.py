e1e36582ec78b4be8a71cf5a938d5347eb85a4ae
shane <shanedm@LSCREATIONx>
2026-02-11 09:54:28 +0100

    linting wip

--- dql_cartpole.py
+++ dql_cartpole.py
@@ -1,51 +1,80 @@
+"""dql_cartpole"""
 # Import necessary libraries
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 import random
 from collections import namedtuple, deque
+import numpy as np
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+import gymnasium as gym
+
+SEED=170715
+# Define training parameters
+NUM_EPISODES = 250
+# NUM_EPISODES = 20
+MAX_STEPS_PER_EPISODE = 200
+EPSILON_START = 1.0
+EPSILON_END = 0.2
+EPSILON_DECAY_RATE = 0.99
+GAMMA = 0.9
+LR = 0.0025
+BUFFER_SIZE = 10000
+buffer = deque(maxlen=BUFFER_SIZE)
+BATCH_SIZE = 128
+UPDATE_FREQUENCY = 10
+
 
 # Check if GPU is available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device( "cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 # Define the neural network model
 class QNetwork(nn.Module):
-    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
-        super(QNetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
+    """QNetwork"""
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        super().__init__()
+        self.seed = torch.manual_seed(SEED)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
         self.to(device)
 
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
+    def forward(self, state_l):
+        """ forward pass"""
+        x = F.relu(self.fc1(state_l))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
 class ReplayBuffer:
-    def __init__(self, action_size, buffer_size, batch_size, seed):
+    """replay buffer"""
+    def __init__(self, action_size, buffer_size_l, batch_size_l):
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)
-        self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
+        self.memory = deque(maxlen=buffer_size_l)
+        self.batch_size = batch_size_l
+        self.experience = namedtuple(
+        "Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        random.seed(SEED)
 
-    def add(self, state, action, reward, next_state, done):
-        e = self.experience(state, action, reward, next_state, done)
+    def add(self, state_l, action_l, reward_l, next_state_l, done_l):
+        """add experience"""
+        e = self.experience(state_l, action_l, reward_l, next_state_l, done_l)
         self.memory.append(e)
 
     def sample(self):
+        """sample experience"""
         experiences = random.sample(self.memory, k=self.batch_size)
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        states = torch.from_numpy(np.vstack(
+        [e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack(
+        [e.action for e in experiences if e is not None])).long().to(device)
+        rewards = torch.from_numpy(np.vstack(
+        [e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack(
+        [e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack(
+        [e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
 
         return (states, actions, rewards, next_states, dones)
 
@@ -54,20 +83,22 @@ class ReplayBuffer:
 
 # Define the DQN agent class
 class DQNAgent:
+    """DQN Agent"""
     # Initialize the DQN agent
-    def __init__(self, state_size, action_size, seed, lr):
+    def __init__(self, state_size, action_size, lr):
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(seed)
+        random.seed(SEED)
 
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_local = QNetwork(state_size, action_size).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size).to(device)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr)
 
-        self.memory = ReplayBuffer(action_size, buffer_size=int(1e5), batch_size=64, seed=seed)
+        self.memory = ReplayBuffer(action_size, buffer_size_l=int(1e5), batch_size_l=64)
         self.t_step = 0
 
     def step(self, state, action, reward, next_state, done):
+        """step"""
         self.memory.add(state, action, reward, next_state, done)
 
         self.t_step = (self.t_step + 1) % 4
@@ -76,10 +107,10 @@ class DQNAgent:
                 experiences = self.memory.sample()
                 self.learn(experiences, gamma=0.99)
 
-    # Choose an action based on the current state
     def act(self, state, eps=0.):
+        """Choose an action based on the current state"""
         state_tensor = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        
+
         self.qnetwork_local.eval()
         with torch.no_grad():
             action_values = self.qnetwork_local(state_tensor)
@@ -87,11 +118,10 @@ class DQNAgent:
 
         if np.random.random() > eps:
             return action_values.argmax(dim=1).item()
-        else:
-            return np.random.randint(self.action_size)
+        return np.random.randint(self.action_size)
 
-    # Learn from batch of experiences
     def learn(self, experiences, gamma):
+        """Learn from batch of experiences"""
         states, actions, rewards, next_states, dones = zip(*experiences)
         states = torch.from_numpy(np.vstack(states)).float().to(device)
         actions = torch.from_numpy(np.vstack(actions)).long().to(device)
@@ -99,12 +129,12 @@ class DQNAgent:
         next_states = torch.from_numpy(np.vstack(next_states)).float().to(device)
         dones = torch.from_numpy(np.vstack(dones).astype(np.uint8)).float().to(device)
 
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        q_targets = rewards + (gamma * q_targets_next * (1 - dones))
 
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        q_expected = self.qnetwork_local(states).gather(1, actions)
 
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = F.mse_loss(q_expected, q_targets)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -112,64 +142,47 @@ class DQNAgent:
         self.soft_update(self.qnetwork_local, self.qnetwork_target, tau=1e-3)
 
     def soft_update(self, local_model, target_model, tau):
+        """soft update"""
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
-# Initialize the environment and the agent
-import gymnasium as gym
-from collections import deque
-import random
 
 # Set up the environment
 env = gym.make("CartPole-v1")
-
-# Define training parameters
-num_episodes = 250
-# num_episodes = 20
-max_steps_per_episode = 200
-epsilon_start = 1.0
-epsilon_end = 0.2
-epsilon_decay_rate = 0.99
-gamma = 0.9
-lr = 0.0025
-buffer_size = 10000
-buffer = deque(maxlen=buffer_size)
-batch_size = 128
-update_frequency = 10
 
 
 # Initialize the DQNAgent
 input_dim = env.observation_space.shape[0]
 output_dim = env.action_space.n
-new_agent = DQNAgent(input_dim, output_dim, seed=170715, lr = lr)
+new_agent = DQNAgent(input_dim, output_dim, lr = LR)
 
 # Training loop
-for episode in range(num_episodes):
+for episode in range(NUM_EPISODES):
     # Reset the environment
     state = env.reset()[0]
-    epsilon = max(epsilon_end, epsilon_start * (epsilon_decay_rate ** episode))
+    epsilon = max(EPSILON_END, EPSILON_START * (EPSILON_DECAY_RATE ** episode))
 
     # Run one episode
     step = 0
-    for step in range(max_steps_per_episode):
+    for step in range(MAX_STEPS_PER_EPISODE):
         # Choose and perform an action
         action = new_agent.act(state, epsilon)
         next_state, reward, done, truncated, _ = env.step(action)
-        
+
         buffer.append((state, action, reward, next_state, done))
-        
-        if len(buffer) >= batch_size:
-            batch = random.sample(buffer, batch_size)
+
+        if len(buffer) >= BATCH_SIZE:
+            batch = random.sample(buffer, BATCH_SIZE)
             # Update the agent's knowledge
-            new_agent.learn(batch, gamma)
-        
+            new_agent.learn(batch, GAMMA)
+
         state = next_state
-        
+
         # Check if the episode has ended
         if done:
             break
-    
-    if (episode + 1) % update_frequency == 0:
+
+    if (episode + 1) % UPDATE_FREQUENCY == 0:
         print(f"Episode {episode + 1}: Finished training, Steps {step}")
 
 # Evaluate the agent's performance
@@ -180,20 +193,18 @@ for episode in range(num_episodes):
     # state = env.reset()[0]
     # episode_reward = 0
     # done = False
-    
+
     # while not done:
         # action = new_agent.act(state, eps=0.)
         # next_state, reward, done, truncated, _ = env.step(action)
         # episode_reward += reward
         # state = next_state
-        
     # episode_rewards.append(episode_reward)
 
 # average_reward = sum(episode_rewards) / test_episodes
 # print(f"Average reward over {test_episodes} test episodes: {average_reward:.2f}")
 
 # Visualize the agent's performance
-import time
 
 env = gym.make("CartPole-v1", render_mode="human")
 
