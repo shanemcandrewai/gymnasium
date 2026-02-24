33d3e9a03bd89646c0ac5e55258550e87ff54140
shane <shanedm@LSCREATIONx>
2026-02-24 12:58:16 +0100

    add source URL2

--- reinforcement_q_learning.py
+++ reinforcement_q_learning.py
@@ -1,6 +1,6 @@
 """
-Reinforcement Learning (DQN) Tutorial
 https://docs.pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+Reinforcement Learning (DQN) Tutorial
 =====================================
 **Author**: `Adam Paszke <https://github.com/apaszke>`_
             `Mark Towers <https://github.com/pseudo-rnd-thoughts>`_
